99a40d40b9d34805e8ccaaf798a3aeeb23e18054
Viqqs <89267977+P-Vikitha@users.noreply.github.com>
2023-03-18 21:36:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { Login } from "./Login";
+import { Signup } from "./Signup";
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Signup onFormSwitch={toggleForm} />
+      }
+    </div>
+  );
+}
+
+export default App;
